4fe7eaf7375f4a275e6e1db05c44cb83200644af
Nihir14 <nihirverma14@gmail.com>
2024-09-30 15:43:22 +0530

    update codes

--- program1.py
+++ program1.py
@@ -4,17 +4,23 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
-
-
-
-
-
-
-
-    
-
-
-
-  
-
+        # Dictionary to map closing brackets to their corresponding opening brackets
+        bracket_map = {')': '(', ']': '[', '}': '{'}
+        stack = []
+        
+        # Iterate through each character in the string
+        for char in s:
+            # If the character is a closing bracket
+            if char in bracket_map:
+                # Check the top of the stack, or set it to a dummy value if stack is empty
+                top_element = stack.pop() if stack else '#'
+                
+                # If the top of the stack does not match the corresponding opening bracket
+                if bracket_map[char] != top_element:
+                    return False
+            else:
+                # It's an opening bracket, push it onto the stack
+                stack.append(char)
+        
+        # If the stack is empty, all opening brackets have been closed properly
+        return not stack
--- program2.py
+++ program2.py
@@ -4,7 +4,26 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
+        # Roman numerals and their values
+        roman_values = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
 
+        # Initialize result
+        result = 0
+        
+        # Iterate through the string
+        for i in range(len(s)):
+            # If this is not the last character and the current numeral is smaller than the next one
+            if i < len(s) - 1 and roman_values[s[i]] < roman_values[s[i + 1]]:
+                result -= roman_values[s[i]]
+            else:
+                result += roman_values[s[i]]
 
-
+        return result
